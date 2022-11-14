34d329882fdee5d5594767b1016244d02c405fcd
BudgieBeep420 <finlaynugent@gmail.com>
2022-11-14 02:40:01 +0000

    Started Graphing Normalised Data
    To do: parse this data further for calibration

--- dataviewer.py
+++ dataviewer.py
@@ -1,22 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
-data1Dir = "8thtest.txt"
-straightLineDir = "STRAIGHTLINE__Data.txt"
-eightLinesDir = "8lines__Data.txt"
-data1finalDir = "8thtest.npy"
-straightLineFinalDir = "STRAIGHTLINE__Data.npy"
-eightLinesFinalDir = "8lines__Data.npy"
+data1Dir = "raw/8thtest.txt"
+straightLineDir = "raw/STRAIGHTLINE__Data.txt"
+eightLinesDir = "raw/8lines__Data.txt"
+
+data1finalDir = "cleandata/8thtest.npy"
+straightLineFinalDir = "cleandata/STRAIGHTLINE__Data.npy"
+eightLinesFinalDir = "cleandata/8lines__Data.npy"
 
 
 def Normalise(dir, finalDir):
-    dataarray=np.loadtxt(dir,skiprows=1, delimiter="\t" )
+    dataarray = np.loadtxt(dir, skiprows=1, delimiter="\t")
     print("First data point:")
     print(dataarray[1])
 
-    startingxdata=dataarray[0:100,1]
-    startingydata=dataarray[0:100,2]
-    startingzdata=dataarray[0:100,3]
+    startingxdata = dataarray[0:100, 1]
+    startingydata = dataarray[0:100, 2]
+    startingzdata = dataarray[0:100, 3]
 
     print("drifts:\n")
 
@@ -24,10 +26,10 @@ def Normalise(dir, finalDir):
     ydrift = np.mean(startingydata)
     zdrift = np.mean(startingzdata)
 
-    print(xdrift, " " , ydrift , " " , zdrift)
-    dataarray[:,1] -= xdrift
-    dataarray[:,2] -= ydrift
-    dataarray[:,3] -= zdrift
+    print(xdrift, " ", ydrift, " ", zdrift)
+    dataarray[:, 1] -= xdrift
+    dataarray[:, 2] -= ydrift
+    dataarray[:, 3] -= zdrift
 
     print("Normalised data:\n")
     print(dataarray[1])
@@ -35,9 +37,9 @@ def Normalise(dir, finalDir):
     np.save(finalDir, dataarray)
 
 
-Normalise(eightLinesDir, eightLinesFinalDir)
-Normalise(data1Dir, data1finalDir)
-Normalise(straightLineDir, straightLineFinalDir)
+# Normalise(eightLinesDir, eightLinesFinalDir)
+# Normalise(data1Dir, data1finalDir)
+# Normalise(straightLineDir, straightLineFinalDir)
 
 
 # %%
@@ -46,17 +48,45 @@ def VelAndDispEuler(accel, t):
     vx = [0]
     dx = [0]
 
-    for i in range(1,len(accel)):
-        vx.append(vx[i-1] + accel[i-1]*(t[i]-t[i-1]))
-        dx.append(dx[i-1] + vx[i-1]*(t[i]-t[i-1]))
+    for i in range(1, len(accel)):
+        vx.append(vx[i - 1] + accel[i - 1] * (t[i] - t[i - 1]))
+        dx.append(dx[i - 1] + vx[i - 1] * (t[i] - t[i - 1]))
 
     return vx, dx
 
+
 # Reading Data
 
-rawData8Lines = np.loadtxt(eightLinesDir, skiprows=1, delimiter="\t")
-normalised8Lines = np.load(eightLinesFinalDir, allow_pickle=True)
+# rawData8Lines = np.loadtxt(eightLinesDir, skiprows=1, delimiter="\t")
 
-rawAccel8Lines, rawVel8Lines = VelAndDispEuler(rawData8Lines[1], rawData8Lines[0])
+correct8Lines = np.load(eightLinesFinalDir, allow_pickle=True)
 
-print(rawVel8Lines)
+eightLineTime = correct8Lines[:, 0]
+eightLineAccel = [correct8Lines[:, 1], correct8Lines[:, 1], correct8Lines[:, 2]]
+eightLineAngVel = [correct8Lines[:, 3], correct8Lines[:, 4], correct8Lines[:, 5]]
+eightLineVel = [[], [], []]
+eightLineDisp = [[], [], []]
+
+for i in range(0, 3):
+    eightLineVel[i], eightLineDisp[i] = VelAndDispEuler(eightLineAccel[i],
+                                                        eightLineTime)
+
+
+def plotAxes(time, data, tag, ylabel):
+    # plt.figure(1)
+    # plt.subplot(211)
+    plt.plot(time, data[0], 'r',
+            time, data[1], 'g',
+            time, data[2], 'b')
+    plt.ylabel(ylabel)
+    plt.legend([tag + '$_x$', tag + '$_y$', tag + '$_z$'], bbox_to_anchor=(1.0, 1.0))
+
+plt.figure(1)
+plt.subplot(311)
+plotAxes(eightLineTime, eightLineAccel, 'a', 'acceleration (ms^-2)')
+plt.subplot(312)
+plotAxes(eightLineTime, eightLineVel, 'v', 'velocity (ms^-1)')
+plt.subplot(313)
+plotAxes(eightLineTime, eightLineDisp, 'd', 'displacement (m)')
+
+plt.show()
